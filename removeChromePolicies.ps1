3b3b54af15256da6d6fb8f34fb1dbde12fe31d34
noaboa97 <30494947+noaboa97@users.noreply.github.com>
2019-10-28 14:31:37 +0100

    Add files via upload

---
+++ removeChromePolicies.ps1
@@ -0,0 +1,4 @@
+﻿$val = Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google -Name * -ErrorAction Ignore
+if ($val -ne $null){
+
+Remove-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\ -Name *}
