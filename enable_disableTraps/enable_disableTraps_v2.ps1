31d881193a2ebf50236f0d87dbb9e73f0e660d09
noaboa97 <30494947+noaboa97@users.noreply.github.com>
2020-06-11 10:47:25 +0200

    rewritten in PowerShell
    Script rewritten in PowerShell. Password is now stored encrypted in the C Drive as an XML. If XML isn't present it prompts to enter the password and saves it.

---
+++ enable_disableTraps/enable_disableTraps_v2.ps1
@@ -0,0 +1,30 @@
+﻿if(Test-Path "$pscriptroot\enable_disableTraps_Cred.xml") {
+  
+    $credential = Import-CliXml -Path  "$pscriptroot\enable_disableTraps_Cred.xml"
+    $credential = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR((($credential))))
+}
+else{
+
+    Read-host "Enter Password" -AsSecureString | Export-CliXml  -Path "$pscriptroot\enable_disableTraps_Cred.xml"
+
+}
+
+$state = (Get-Service -Name "CyveraService").Status
+
+if($state -eq "Running"){
+
+
+
+cd "c:\Program Files\Palo Alto Networks\Traps\"
+cmd.exe /c "echo $credential| cytool runtime stop"
+
+}
+else{
+
+cd "c:\Program Files\Palo Alto Networks\Traps\"
+cmd.exe /c cytool runtime start
+
+}
+
+start-sleep 5
+
