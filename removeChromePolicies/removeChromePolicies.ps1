023fda64ccda48a604f8bee6e9b2b6d28445acaa
noaboa97 <30494947+noaboa97@users.noreply.github.com>
2020-09-02 13:58:20 +0200

    enhancement
    Added a logfile

--- removeChromePolicies/removeChromePolicies.ps1
+++ removeChromePolicies/removeChromePolicies.ps1
@@ -1,4 +1,16 @@
+﻿$Logfile = "C:\Temp\PS Scripts\Logs\removeChromePolicies.log"
 $val = Get-Item -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome -ErrorAction Ignore
 if ($val -ne $null){
 
-Remove-Item -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome -Recurse}
+    Remove-Item -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome -Recurse
+
+    if((Get-Item -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome -ErrorAction Ignore) -eq $null){
+        Add-Content -Path $Logfile -Value "$(get-date -Format "dd.MM.yyyy_HH.mm.ss"): Chrome Policies deleted"
+    }
+    
+}
+else {
+
+    Add-Content -Path $Logfile -Value "$(get-date -Format "dd.MM.yyyy_HH.mm.ss"): No Chrome Policies found"
+
+}
